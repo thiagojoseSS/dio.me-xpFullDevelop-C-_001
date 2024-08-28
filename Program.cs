cc0d153e505640f50c73f5476f0e6ccde7e15128
thiagojoseSS <thiagoesoj@gmail.com>
2024-08-27 22:46:38 -0300

    feat: instanciando uma class em C#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using ExemploFundamentos.Models;
+
+Pessoal pessoa = new Pessoal();
+
+pessoa.Nome = "Thiago";
+pessoa.Idade = 27;
+pessoa.Apresentar();
