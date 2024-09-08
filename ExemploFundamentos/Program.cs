6da86da563ddbad60d49d0959ecfdc8cb744cb5e
thiagojoseSS <thiagoesoj@gmail.com>
2024-09-08 19:58:29 -0300

    feat: learned For and Foreach

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -9,9 +9,17 @@ arrayInteiros[0] = 1;
 arrayInteiros[1] = 2;
 arrayInteiros[2] = 3;
 
+Console.WriteLine("Exemplo For:");
 for (int contador = 0; contador < arrayInteiros.Length; contador++)
 {
     Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+}
+
+Console.WriteLine("Exemplo Foreach:");
+int contadorForeach = 0;
+foreach (int valor in arrayInteiros) {
+    Console.WriteLine($"Posição Nº {contadorForeach} - {valor}");
+    contadorForeach++;
 }
 
 
